25c9ba78dd3c212645d290175c3b092bf69aec27
Hizrael19 <juwonolutoki12@gmail.com>
2022-05-17 23:48:54 +0100

    all commit

---
+++ app.py
@@ -0,0 +1,134 @@
+# import random
+# secret_number = random.randint(1, 10)
+# guess_limit = 10
+# guess_count = 0
+#
+# # <------Guessing Game----------->
+#
+# while guess_count < guess_limit:
+#     guess = int(input("Guess a number: "))
+#     guess_count += 1
+#     if guess > secret_number:
+#         print("Ouch! That's too high!")
+#         guess_count += 0
+#     elif guess < secret_number:
+#         print("Ouch! That's too low!")
+#         # guess_count += 1
+#     elif guess == secret_number:
+#         print(f"You Won! You made {guess_count} attempt")
+#         break
+# else:
+#     print(f"You lose! You made {guess_count} attempts")
+#
+#
+#
+#
+#
+#
+
+# info = str(input(">: ")).lower()
+# if info == "help":
+#     print("""
+#     Start - to start the car
+#     Stop - to stop the car
+#     quit - to quit""")
+
+# command = ""
+# started = false
+# while True:
+#     command = str(input(">: ")).lower()
+#     if command == "start":
+#         print("The car has started...Ready to go!")
+#     elif "start" == "start":
+#         print("Car has already started....")
+#     elif command == "stop":
+#         print("The car has stopped!")
+#     elif command == "help":
+#         print("""
+#             Start - to start the car
+#             Stop - to stop the car
+#             quit - to quit""")
+#     elif command == "quit":
+#         break
+#     else:
+#         print("I don't understand these characters")
+
+
+# prices = [10, 20, 30]
+#
+# total = 0
+# for price in prices:
+#     total += price
+# print(f"The total price is: {total}")
+
+
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x}, {y})")
+
+
+
+# numbers = [2, 2, 2, 2, 7 ]
+# for item in numbers:
+#     output = ""
+#     for count in range(item):
+#         output += "x"
+#     print(output)
+
+
+# numbers = [2, 5, 7, 3, 3]
+# max = numbers[0]
+# for number in numbers:
+#     if number > max:
+#         max = number
+# print(max)
+
+
+
+
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# print(matrix[0][1])
+#
+#\
+
+
+
+# numbers = [5, 2, 1, 5,  7, 4]
+# numbers.remove(5)
+# print(numbers)
+
+
+customer = {
+    "name": "John Smith",
+    "age": 30,
+    "is_verified": True
+    
+}
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
